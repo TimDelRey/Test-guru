87d2743b6d94a4837a7f05e38f7d9a05b4639b2f
TimDelRey <kepaliga@mail.ru>
2024-11-10 00:25:34 +0300

    feat, добавлены хэперы года и ссылки на гитхаб в футтер, а также хелперы заголовков формы и счетчик вопросов

--- app/helpers/questions_helper.rb
+++ app/helpers/questions_helper.rb
@@ -1,2 +1,10 @@
 module QuestionsHelper
+  def question_header(test)
+    form_title = ["В категории '#{test.title}' создать вопрос", "В категории '#{test.title}' редактировать вопрос"]
+    if @question.persisted?
+      form_title[1]
+    else
+      form_title[0]
+    end
+  end
 end
