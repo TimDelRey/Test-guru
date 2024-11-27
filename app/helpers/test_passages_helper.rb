b476966193eb8ade00b749a3cb43ea1f39d062d6
TimDelRey <kepaliga@mail.ru>
2024-11-27 20:36:29 +0300

    feat,добавлена логика прохождения теста, двухцветный вывод результата прохождения теста

--- app/helpers/test_passages_helper.rb
+++ app/helpers/test_passages_helper.rb
@@ -1,2 +1,15 @@
 module TestPassagesHelper
+  def test_result(test_passage)
+    pass_percent = 85.freeze
+    result_factor = test_passage.correct_questions.to_f / test_passage.test.questions.count
+    test_result_percent = result_factor * 100
+
+    if test_result_percent >= pass_percent
+      result = "<span class='result-green'>Тест успешно пройден</span>"
+    else 
+      result = "<span class='result-red'>Тест не пройден</span>"
+    end
+
+    "#{result}<br>#{test_result_percent.to_i}% правильных ответов".html_safe
+  end
 end
--- app/models/test_passage.rb
+++ app/models/test_passage.rb
@@ -25,9 +25,15 @@ class TestPassage < ApplicationRecord
   end
 
   def correct_answer?(answer_ids)
-    correct_answer_count = correct_answers.count
-    (correct_answer_count== correct_answers.where(id: answer_ids).count) &&
-    correct_answer_count == answer_ids.count
+    correct_answers = current_question.answers.correct
+
+    puts "Правильные ответы: #{correct_answers.ids.sort}"
+    puts "Ответы полученные Answer IDs: #{answer_ids.to_a.map(&:to_i).sort}"
+  # byebug
+    correct_answers.ids.sort == answer_ids.to_a.map(&:to_i).sort
+    # correct_answer_count = correct_answers.count
+    # (correct_answer_count == correct_answers.where(id: answer_ids).count) &&
+    # correct_answer_count == answer_ids.count
   end
 
   def correct_answers
